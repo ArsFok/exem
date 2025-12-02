0df9a6e070a5978b35c5f3744ff48a0c8a7bccfc
ArsFok <arsenyfok@yandex.ru>
2025-12-02 18:45:59 +0300

    develop Реализовать программу выводящую список файлов в директории(переданной в качестве первого аргумента ком строки) в порядке убывания их размера

---
+++ new_exam/line.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+
+void sort(char a, char b){
+
+}
+
+int main(int argc, char **argv){
+    std::vector line;
+    if(argc < 1){
+        std::cout << "Invalid argum" << std::endl;
+    }
+    else if(argc == 1){
+        std::string = argv;
+    }
+}
