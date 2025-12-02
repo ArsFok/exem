18c16775fa59ee20b77eb788fd3f10e62d6a8a58
ArsFok <arsenyfok@yandex.ru>
2025-12-02 18:31:58 +0300

    develop реализуйте прогу котрая использует алгоритм std::transform для преобразования эл-ов вектора с использованием лямбда функции. Программа должна складывать каждый элемент вектора с максимальным э-ом вектора на момент сложения

---
+++ new_exam/transform.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+void transfom(int a){
+    for(int i = 0; i < 0; i++){
+        if(a > 0){
+            std::
+        }
+    }
+}
+int main(){
+    std::vector arr;
+    while(true){
+        
+        std::cin << a;
+
+    }
+
+}
